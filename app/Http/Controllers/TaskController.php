eeea16fb41b964184141ba72147a71f9b6f35505
Adrian Drysdale <adrian.drysdale@visualsoft.co.uk>
2024-06-26 23:05:46 +0100

    clean up backend

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,30 +5,30 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreTaskRequest;
 use App\Http\Requests\UpdateTaskRequest;
 use App\Models\Task;
+use \Illuminate\Http\RedirectResponse;
 
 class TaskController extends Controller
 {
     public function index()
     {
-        $tasks = Task::all();
-        return view('tasks', compact('tasks'));
+        return view('tasks', ['tasks' => Task::all()]);
     }
 
 
-    public function store(StoreTaskRequest $request)
+    public function store(StoreTaskRequest $request): RedirectResponse
     {
         Task::create(['name' => $request->name]);
         return redirect()->route('tasks.index')->with('message', 'Task added successfully');
     }
 
-    public function update(UpdateTaskRequest $request, Task $task)
+    public function update(UpdateTaskRequest $request, Task $task): RedirectResponse
     {
-        $task->completed = $request->completed;
+        $task->fill($request->validated());
         $task->save();
         return redirect()->route('tasks.index')->with('message', 'Task marked as complete');
     }
 
-    public function destroy(Task $task)
+    public function destroy(Task $task): RedirectResponse
     {
         $task->delete();
         return redirect()->route('tasks.index')->with('message', 'Task successfully deleted');
